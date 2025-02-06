ec91c6aab79366d122c7a53e713a0187b676c393
amalthobyy <amalpthobias@gmail.com>
2025-02-06 15:28:21 +0530

    firebaseAuthnotworking

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { toast } from "react-toastify";
 
 
 
@@ -38,7 +39,7 @@ const signup = async (name, email, password) => {
         
     } catch (error) {
         console.error("Signup error:", error);
-        alert(error.message);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
     }
 };
 
@@ -48,7 +49,7 @@ const login= async (email,password) =>{
 
     }catch (error){
         console.log(error);
-        alert(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
 
     }
 
